5c75cdb6d0c8322071448baf9d5b54ae0960b6e3
Itamar Katz <itamar.katz@targatelematics.com>
2024-07-11 10:25:42 +0000

    neovim: disable mouse in vim-options.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set mouse=")
 vim.cmd("nnoremap <tab> <c-w><c-w>")
 --vim.cmd("hi CursorLine cterm=NONE ctermbg=24 ctermfg=white guibg=darkred guifg=white")
 vim.g.mapleader = " "
