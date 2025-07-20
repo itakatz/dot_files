87467f2691db30c430383a498162b4bf82d122bd
Itamar Katz <itakatz@gmail.com>
2025-07-20 14:05:54 +0300

    added shift-tab map to go back a split

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -4,7 +4,8 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set mouse=")
-vim.cmd("nnoremap <tab> <c-w><c-w>")
+vim.cmd("nnoremap <tab> <c-w>w")
+vim.cmd("nnoremap <s-tab> <c-w>W")
 --vim.cmd("hi CursorLine cterm=NONE ctermbg=24 ctermfg=white guibg=darkred guifg=white")
 vim.g.mapleader = " "
 
